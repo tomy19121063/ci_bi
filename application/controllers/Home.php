9c951268131cae445d6b8ac7fdeba5a74ffbc5a4
mashan-lomastxt <riska.handika@students.amikom.ac.id>
2021-11-11 22:08:25 +0700

    edit logout

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -16,7 +16,7 @@ class Home extends CI_Controller {
     public function logout()
     {
         $this->session->session_destroy();
-        redirect('login');
+        redirect('home/logout');
     }
 
 }
